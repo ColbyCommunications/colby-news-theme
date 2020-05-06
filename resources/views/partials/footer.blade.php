578bd0450bd23629809d3a36247d9460ee484ea0
Brandon K. Waltz <bkwaltz@colby.edu>
2020-05-06 14:52:23 -0400

    responsive styles

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
   </div>
 </footer> -->
 
-<footer style="background-color: #353535; min-height: 200px; margin-top:3rem; color: #fff;" class="d-flex">
+<footer class="in-the-news-footer d-flex">
   <div class="container d-flex align-items-center justify-content-between">
     <div><img src="{{ \App\asset_path('images/COLBY_logotype_white.png') }}" /></div>
     <div class="in-the-news-footer-menu">@if (has_nav_menu('footer_navigation'))
