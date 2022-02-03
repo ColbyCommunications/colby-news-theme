25c4c1eed269c29cfc8c19963b694b4cc05bd331
Brandon K. Waltz <bwaltz6@gmail.com>
2022-02-03 15:14:19 -0500

    updates

--- webpack-default.mix.js
+++ webpack-default.mix.js
@@ -2,54 +2,59 @@ const mix = require('laravel-mix');
 const glob = require('glob');
 const tailwindcss = require('tailwindcss');
 require('mix-env-file');
-require("laravel-mix-polyfill");
-require("@ayctor/laravel-mix-svg-sprite");
-require("laravel-mix-string-replace");
+require('laravel-mix-polyfill');
+require('@ayctor/laravel-mix-svg-sprite');
+require('laravel-mix-string-replace');
 require('laravel-mix-copy-watched');
 
 exports.buildPaths = (overrides = {}) => {
   const defaultPaths = {
-    icons: "icons",
-    scss: "scss",
-    js: "js",
-  }
+    icons: 'icons',
+    scss: 'scss',
+    js: 'js',
+  };
 
   const fixedPaths = {
-    srcRoot: "src",
-    dist: "dist",
-    fonts: "fonts",
-    images: "images",
-  }
+    srcRoot: 'pattern-library/src',
+    dist: 'dist',
+    fonts: 'fonts',
+    images: 'images',
+  };
 
   const newPaths = Object.assign(defaultPaths, overrides);
   return Object.assign(newPaths, fixedPaths);
 };
 
 exports.spritePrefixer = (paths) => (fullPath) => {
-
   const iconRoot = paths.icons ? paths.icons : paths.srcRoot;
 
-  const pathParts = fullPath.split("/");
+  const pathParts = fullPath.split('/');
   const rootIndex = pathParts.lastIndexOf(iconRoot);
 
   const prefixes = pathParts.slice(rootIndex + 1, -1);
 
   if (!prefixes.length) {
-    return "";
+    return '';
   }
 
-  return prefixes.join("-") + "-";
+  return prefixes.join('-') + '-';
 };
 
 exports.defaultMix = (paths) => {
   return mix
     .env(process.env.ENV_FILE)
     .extract()
-    .copyDirectoryWatched(`${paths.srcRoot}/${paths.images}`, `${paths.dist}/images`)
-    .copyDirectoryWatched(`${paths.srcRoot}/${paths.fonts}`, `${paths.dist}/fonts`)
-    // .polyfill({
-    //   enabled: false,
-    //   useBuiltIns: "usage",
-    //   targets: false,
-    // });
-}
+    .copyDirectoryWatched(
+      `${paths.srcRoot}/${paths.images}`,
+      `${paths.dist}/images`
+    )
+    .copyDirectoryWatched(
+      `${paths.srcRoot}/${paths.fonts}`,
+      `${paths.dist}/fonts`
+    );
+  // .polyfill({
+  //   enabled: false,
+  //   useBuiltIns: "usage",
+  //   targets: false,
+  // });
+};
