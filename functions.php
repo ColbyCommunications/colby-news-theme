e8c7299088fe657fce6898de742a57675cf3b38c
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-03-30 15:47:44 -0400

    test

--- functions.php
+++ functions.php
@@ -1062,7 +1062,7 @@ function page_metrics_function() {
             update_post_meta($id, 'siteimprove_page_views', 0);
         }
     }
-    WP_CLI::runcommand('algolia reindex');
+    WP_CLI::runcommand('algolia reindex prod_news_searchable_posts');
 }
 
 /**
