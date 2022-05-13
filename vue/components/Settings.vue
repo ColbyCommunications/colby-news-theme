8d4f1fa2bde3ccca1a3aed50cbe344a8b3e7414a
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-05-13 15:13:58 -0400

    restyle filters modal on stories tab

--- vue/components/Settings.vue
+++ vue/components/Settings.vue
@@ -11,10 +11,11 @@
     </div>
     <!-- filter modal for stories -->
     <div v-show="checkTabStories" class="bg-gray-200 px-8 py-8">
-      <h2 class="text-2xl">Category</h2>
+      <h2 class="text-lg mb-4">Category</h2>
       <ais-refinement-list
         attribute="primary_category"
         :sort-by="['name:desc']"
+        :transform-items="transformItems"
         show-more
       />
     </div>
@@ -37,11 +38,40 @@ export default {
     toggleFilters() {
       this.isOpen = !this.isOpen;
     },
+    transformItems(items) {
+      return items.map((item) => ({
+        ...item,
+        count: `(${item.count})`,
+      }));
+    },
   },
 };
 </script>
-<style scoped>
+<style>
 .filters-icon {
   font-size: 28px;
+}
+
+.ais-RefinementList-list {
+  margin-bottom: 1rem;
+}
+
+.ais-RefinementList-label {
+  display: flex;
+  align-items: center;
+}
+
+.ais-RefinementList-checkbox {
+  transform: scale(1.3);
+  border: 1px solid rgb(107 114 128);
+}
+
+.ais-RefinementList-labelText {
+  font-size: 1rem;
+  padding-left: 0.5rem;
+}
+
+.ais-RefinementList-count {
+  padding-left: 0.5rem;
 }
 </style>
