96df19c5d61e6b6d8894c148ca3b53ff05a1f2d3
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-05-31 10:54:27 -0400

    add sliding teacher to no results page

--- vue/components/AllResults.vue
+++ vue/components/AllResults.vue
@@ -10,13 +10,13 @@
       <ais-index index-name="prod_news_searchable_posts" index-id="noresult">
         <ais-configure
           :filters="'post_type:post'"
-          :hits-per-page.camel="10"
+          :hits-per-page.camel="12"
           query=""
         />
         <div class="wp-block">
           <div class="wp-block nc-slider-with-teaser-pair">
-            <div class="mb-10">
-              <ais-hits :transform-items="getTeaserPair">
+            <div>
+              <ais-hits class="mb-10" :transform-items="getTeaserPair">
                 <template v-slot="{ items }">
                   <div class="grid md:grid-cols-2 gap-8">
                     <div v-for="item in items" :key="item.objectID">
@@ -42,10 +42,55 @@
                   </div>
                 </template>
               </ais-hits>
-
+              <!-- slider -->
               <ais-hits :transform-items="getTeaserSlider">
-                <template v-slot:item="{ item }">
-                  {{ item.post_title }}
+                <template v-slot="{ items }">
+                  <div class="">
+                    <div class="">
+                      <div class="">
+                        <div class="-mx-container-gutter" style="">
+                          <ul
+                            class="flex space-x-8 overflow-x-auto sliding-teasers pl-container-gutter"
+                            style="
+                              scroll-snap-type: x mandatory;
+                              scroll-behavior: smooth;
+                            "
+                          >
+                            <!-- teaser -->
+                            <li
+                              v-for="item in items"
+                              :key="item.objectID"
+                              class="sliding-teaser min-h-[6.5rem] min-w-[16rem] max-w-[16rem]"
+                            >
+                              <div class="cursor-pointer">
+                                <div class="relative group">
+                                  <div
+                                    class="after:content-empty after:absolute after:inset-0 after:bg-black after:bg-opacity-0 group-hover:after:bg-opacity-10 after:transition-colors"
+                                  >
+                                    <img
+                                      width="1080"
+                                      height="720"
+                                      :src="item.images.teaser_new.url"
+                                      class="attachment-teaser_new size-teaser_new"
+                                      sizes="(max-width: 1080px) 100vw, 1080px"
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="pt-1 text-sm uppercase">
+                                {{ item.primary_category }}
+                              </div>
+                              <h3 class="font-bold">
+                                <a :href="item.permalink">{{
+                                  item.post_title
+                                }}</a>
+                              </h3>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </template>
               </ais-hits>
             </div>
@@ -104,7 +149,7 @@ export default {
       return items.slice(0, 2);
     },
     getTeaserSlider(items) {
-      return items.slice(2, items.length - 1);
+      return items.slice(2, items.length + 1);
     },
   },
 
