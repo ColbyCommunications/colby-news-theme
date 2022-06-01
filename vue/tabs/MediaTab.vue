02f38887177c997338453ad40a831a96c84ef6e9
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-06-01 14:05:14 -0400

    add hover effects to Media Coverage tab

--- vue/tabs/MediaTab.vue
+++ vue/tabs/MediaTab.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in items" :key="item.objectID">
               <a
-                class="group block text-base-minus-2 space-y-1.5"
+                class="group block text-base-minus-2 space-y-1.5 hover:text-link-hover"
                 :href="item.external_url"
                 @click="sendEvent('click', item, 'External Media Clicked')"
               >
@@ -26,16 +26,14 @@
                   <div class="!w-1/8 !m-0 !p-0 flex justify-center">
                     <img class="external-image" :src="item.external_image" />
                   </div>
-                  <div class="w-7/8 pl-6">
-                    <h3 class="font-sans text-xs mb-1.5 uppercase">
+                  <div class="w-7/8">
+                    <h3 class="pl-6 font-sans text-xs mb-1.5 uppercase">
                       {{ item.media_source }}
                     </h3>
-                    <h2
-                      class="group-hover:text-link-hover transition-colors font-bold text-base mb-1.5"
-                    >
+                    <h2 class="pl-6 font-bold text-base mb-1.5">
                       <ais-highlight attribute="post_title" :hit="item" />
                     </h2>
-                    <p class="font-sans text-base">
+                    <p class="pl-6 font-sans text-base">
                       <ais-snippet attribute="content" :hit="item" />
                     </p>
                   </div>
