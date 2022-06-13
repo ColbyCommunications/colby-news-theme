def85efb7b32e3939041d81c1905d8d2c79fdb44
Brandon K. Waltz <bwaltz6@gmail.com>
2022-06-13 10:05:44 -0400

    fully remove vue-autosuggest

--- vue/main.js
+++ vue/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue';
 import InstantSearch from 'vue-instantsearch/vue3/es';
 import App from './App.vue';
-import VueAutosuggest from 'vue-autosuggest';
 
 const app = createApp(App);
 app.use(InstantSearch);
-app.use(VueAutosuggest);
 app.mount('#vue');
