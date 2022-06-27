fe537ef80c9578bb3491912e259b85d9fb318624
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-06-27 14:23:59 -0400

    change

--- vue/components/NoResults.vue
+++ vue/components/NoResults.vue
@@ -108,6 +108,7 @@
                         xmlns="http://www.w3.org/2000/svg"
                         fill-rule="evenodd"
                         clip-rule="evenodd"
+                        role="img"
                       >
                         <path
                           d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"
@@ -126,6 +127,7 @@
                         xmlns="http://www.w3.org/2000/svg"
                         fill-rule="evenodd"
                         clip-rule="evenodd"
+                        role="img"
                       >
                         <path
                           d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"
